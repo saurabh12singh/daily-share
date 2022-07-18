a119e711bed292ca146c6a102114bbe8f939402d
saurabh12singh <61622700+saurabh12singh@users.noreply.github.com>
2022-07-18 09:41:34 +0530

    Update ShareDetails.java

--- src/main/java/com/cognizant/portfolio_management/DailySharePrice/model/ShareDetails.java
+++ src/main/java/com/cognizant/portfolio_management/DailySharePrice/model/ShareDetails.java
@@ -4,9 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-/*
- * This JPA Model Class is responsible for handling databases
- */
+
 @Entity
 @Table
 public class ShareDetails {
